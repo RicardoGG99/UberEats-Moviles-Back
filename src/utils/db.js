0fe561dc2c2502576a02eb0b5d2173d83fde8d0d
Ricardo <ricardojgraziano14@gmail.com>
2021-07-25 13:27:38 -0400

    credentials-without-.env

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
-require("dotenv").config({ path: "../../../.env" });
+// require("dotenv").config({ path: "../../../.env" });
 
 const db = new Pool({
-  host: process.env.HOST,
-  database: process.env.DB,
-  user: process.env.USER,
-  port: process.env.DBPORT,
-  password: process.env.PASSWORD,
+  host: "ec2-3-224-7-166.compute-1.amazonaws.com",
+  database: "dcl8ms23tsb5fs",
+  user: "cbyrnnmpcdlcqt",
+  port: "5432",
+  password: "111c00191b82c66c498e9deaadec7d0e880f323f104b8911c0a86a0b63246ee9",
   ssl: {
     rejectUnauthorized: false,
   },
